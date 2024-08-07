120b9a339a50ee5ae99d532a2be2c04b6d16ecf8
asm-2000 <ashim.baral@bajratechnologies.com>
2024-08-07 14:45:46 +0545

    feat: define properties needed for sign in page in corresponding viewmodel

--- FlamingFork/ViewModels/UserLoginViewModel.cs
+++ FlamingFork/ViewModels/UserLoginViewModel.cs
@@ -1,14 +1,42 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace FlamingFork.ViewModels
 {
-    public partial class UserLoginViewModel: ObservableObject
+    public partial class UserLoginViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private string _Email;
+
+        [ObservableProperty]
+        private string _Password;
+
+        [ObservableProperty]
+        private string _EmailError;
+
+        [ObservableProperty]
+        private string _PasswordError;
+
         private INavigation _Navigation;
 
         public UserLoginViewModel(INavigation navigation)
         {
             _Navigation = navigation;
+        }
+
+        [RelayCommand]
+        public void ValidateEmail()
+        {
+        }
+
+        [RelayCommand]
+        public void ValidatePassword()
+        {
+        }
+
+        [RelayCommand]
+        public void ValidateForm()
+        {
         }
     }
 }
