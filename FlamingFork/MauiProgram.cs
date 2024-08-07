8774256f462798404071ccdbc19301b3728edfaa
asm-2000 <ashim.baral@bajratechnologies.com>
2024-08-07 13:25:38 +0545

    feat: implement proper colors and styles for app theme

--- FlamingFork/MauiProgram.cs
+++ FlamingFork/MauiProgram.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.Logging;
-
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
+#if ANDROID
+using Android.Content.Res;
+#endif
 namespace FlamingFork
 {
     public static class MauiProgram
@@ -9,6 +12,7 @@ namespace FlamingFork
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
@@ -18,6 +22,27 @@ namespace FlamingFork
 #if DEBUG
     		builder.Logging.AddDebug();
 #endif
+            Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(Entry), (handler, view) =>
+            {
+#if ANDROID
+
+                handler.PlatformView.BackgroundTintList = ColorStateList.ValueOf(Android.Graphics.Color.Transparent);
+#endif
+            });
+            Microsoft.Maui.Handlers.DatePickerHandler.Mapper.AppendToMapping(nameof(DatePicker), (handler, view) =>
+            {
+#if ANDROID
+
+                handler.PlatformView.BackgroundTintList = ColorStateList.ValueOf(Android.Graphics.Color.Transparent);
+#endif
+            });
+            Microsoft.Maui.Handlers.PickerHandler.Mapper.AppendToMapping(nameof(Picker), (handler, view) =>
+            {
+#if ANDROID
+
+                handler.PlatformView.BackgroundTintList = ColorStateList.ValueOf(Android.Graphics.Color.Transparent);
+#endif
+            });
 
             return builder.Build();
         }
---
+++ FlamingFork/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Maui.Core.Platform;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace FlamingFork.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
