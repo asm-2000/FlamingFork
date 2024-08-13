f7cafb4165a01a0b1bec4346ba0e47ab515e47ae
asm-2000 <ashim.baral@bajratechnologies.com>
2024-08-13 13:20:30 +0545

    feat: implement helper class to store and retrieve authentication token and customer information

--- FlamingFork/Helper/Utilities/SecureStorageHandler.cs
+++ FlamingFork/Helper/Utilities/SecureStorageHandler.cs
@@ -1,7 +1,59 @@
-﻿namespace FlamingFork.Helper.Utilities
+﻿using FlamingFork.Models;
+using System.Text;
+using System.Text.Json;
+
+namespace FlamingFork.Helper.Utilities
 {
-    public class SecureStorageHandler
+    public static class SecureStorageHandler
     {
-#
+        #region Token Storage Handler
+
+        public static async void StoreAuthenticationToken(LoginResponseModel loginResponse)
+        {
+            string? authToken = loginResponse.Token;
+            CustomerModel? customer = loginResponse.CustomerDetails;
+            string customerDetails = JsonSerializer.Serialize(customer);
+            await SecureStorage.SetAsync("Token", authToken);
+            await SecureStorage.SetAsync("CustomerDetails", customerDetails);
+        }
+
+        #endregion Token Storage Handler
+
+        #region Token Returner
+
+        public static async Task<string> GetAuthenticationToken()
+        {
+            try
+            {
+                string? token = await SecureStorage.GetAsync("Token");
+                return token;
+            }
+            catch
+            {
+                return "Not Found";
+            }
+        }
+
+        #endregion Token Returner
+
+        #region Customer Details returner
+
+        public static async Task<CustomerModel> GetCustomerDetails()
+        {
+            string? customer = await SecureStorage.GetAsync("CustomerDetails");
+            CustomerModel? customerDetails = JsonSerializer.Deserialize<CustomerModel>(customer);
+            return customerDetails;
+        }
+
+        #endregion Customer Details returner
+
+        #region Storage Clearer
+
+        public static async void ClearStorage()
+        {
+            SecureStorage.RemoveAll();
+        }
+
+        #endregion Storage Clearer
     }
 }
