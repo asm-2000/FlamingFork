9cf8e433f56349cc6f8614d22fae17f51d9c9f64
asm-2000 <ashim.baral@bajratechnologies.com>
2024-08-14 11:47:48 +0545

    feat: initialize necessary models for fetching and showcasing the menuitems

---
+++ FlamingFork/Models/CartItemModel.cs
@@ -0,0 +1,21 @@
+﻿namespace FlamingFork.Models
+{
+    public class CartItemModel
+    {
+        public int? CartItemId { get; set; }
+        public int CustomerId { get; set; }
+        public string? CartItemName { get; set; }
+        public int CartItemPrice { get; set; }
+        public int Quantity { get; set; }
+
+        public CartItemModel(int customerId, string cartItemName, int cartItemPrice, int quantity) 
+        {
+            CartItemId = null;
+            CustomerId = customerId;
+            CartItemName = cartItemName;
+            CartItemPrice = cartItemPrice;
+            Quantity = quantity;
+        }
+
+    }
+}
---
+++ FlamingFork/Models/MenuItemModel.cs
@@ -0,0 +1,12 @@
+﻿namespace FlamingFork.Models
+{
+    public class MenuItemModel
+    {
+        public int ItemId { get; set; }
+        public string? ItemName { get; set; }
+        public int ItemPrice { get; set; }
+        public string? ItemDescription { get; set; }
+        public string? ItemCategory { get; set; }
+        public string? ItemImageUrl { get; set; }
+    }
+}
