edb01090897d2f13626527e7edc309c2db8c735c
asm-2000 <ashim.baral@bajratechnologies.com>
2024-08-14 12:21:31 +0545

    feat: implement API service to fetch menu items

--- FlamingFork/Repositories/ApiServices/AuthenticationServiceRepository.cs
+++ FlamingFork/Repositories/ApiServices/AuthenticationServiceRepository.cs
@@ -91,5 +91,58 @@ namespace FlamingFork.Repositories.ApiServices
         }
 
         #endregion Login Handler API Service
+
+        #region Menu Items Fetcher
+
+        public async Task<List<CartItemModel>> GetMenuItems()
+        {
+            List<MenuItemModel>? fetchedMenuItems = [];
+            List<CartItemModel>? menuItems = [];
+            ApiResponseMessageModal? ErrorResponse = new();
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+            try
+            {
+                var uri = new Uri("http://" + _Address + "/menu/allMenuItems");
+                var response = await _HttpClient.GetAsync(uri);
+
+                // Tries to deserialize the response to List<MenuItemModel> in case of sucessful fetch.
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    fetchedMenuItems = JsonSerializer.Deserialize<List<MenuItemModel>>(responseBody, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                    // Fetch customerId from secure storage.
+                    CustomerModel customerDetails = await SecureStorageHandler.GetCustomerDetails();
+                    int customerId = Convert.ToInt16(customerDetails.CustomerID);
+                    // Maps details from MenuItemModel to CartItemModel for simplifying add to cart
+                    // functionality in Main Page
+
+                    foreach (MenuItemModel menuitem in fetchedMenuItems)
+                    {
+                        CartItemModel correspodingCartItem = new(customerId, menuitem.ItemName, menuitem.ItemPrice, 1);
+                        menuItems.Add(correspodingCartItem);
+                    }
+
+                    return menuItems;
+                }
+                // Deserializes the response to ApiResponseMessageModel in case of error status.
+                else
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    ErrorResponse = JsonSerializer.Deserialize<ApiResponseMessageModal>(responseBody, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                    return [];
+                }
+            }
+            // Returns empty list if communication with API fails.
+            catch
+            {
+                return [];
+            }
+        }
+
+        #endregion Menu Items Fetcher
     }
 }
