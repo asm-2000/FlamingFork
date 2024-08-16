dbc75128592f0e06e0c72d03e1fff35d7ce6e791
asm-2000 <ashim.baral@bajratechnologies.com>
2024-08-16 21:24:03 +0545

    fix: add missing property (CartItemImageUrl) in CartItemModel

--- FlamingFork/Models/CartItemModel.cs
+++ FlamingFork/Models/CartItemModel.cs
@@ -7,14 +7,16 @@
         public string? CartItemName { get; set; }
         public int CartItemPrice { get; set; }
         public int Quantity { get; set; }
+        public string? CartItemImageUrl { get; set; }
 
-        public CartItemModel(int customerId, string cartItemName, int cartItemPrice, int quantity) 
+        public CartItemModel(int customerId, string cartItemName, int cartItemPrice, int quantity, string cartItemImageUrl) 
         {
             CartItemId = null;
             CustomerId = customerId;
             CartItemName = cartItemName;
             CartItemPrice = cartItemPrice;
             Quantity = quantity;
+            CartItemImageUrl = cartItemImageUrl;
         }
 
     }
