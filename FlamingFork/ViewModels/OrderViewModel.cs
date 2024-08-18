b20dd7d39c97735cee1352415c8919defbc396e7
asm-2000 <ashim.baral@bajratechnologies.com>
2024-08-18 21:14:27 +0545

    feat: implement prototype UI for listing each customer order details

--- FlamingFork/ViewModels/OrderViewModel.cs
+++ FlamingFork/ViewModels/OrderViewModel.cs
@@ -1,8 +1,34 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Diagnostics;
 
 namespace FlamingFork.ViewModels
 {
     public partial class OrderViewModel: ObservableObject
     {
+        [ObservableProperty]
+        private string _IsFetching;
+
+        [ObservableProperty]
+        private string _HasFetched;
+
+        [ObservableProperty]
+        private string _OrdersNotPresent;
+
+        [ObservableProperty]
+        private string _OrdersPresent;
+
+        public OrderViewModel()
+        {
+            _IsFetching = "False";
+            _HasFetched = "True";
+            _OrdersNotPresent = "False";
+        }
+
+        [RelayCommand]
+        public void ChangeOrderVisibilityByType()
+        {
+            Debug.WriteLine("Command Called!");
+        }
     }
 }
