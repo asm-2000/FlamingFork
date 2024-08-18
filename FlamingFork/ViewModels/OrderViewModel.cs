a272ab2b6e8320b6a6369ab81758893204247c16
asm-2000 <ashim.baral@bajratechnologies.com>
2024-08-18 22:42:03 +0545

    feat: implement method in OrderViewModel to fetch all the customer orders

--- FlamingFork/ViewModels/OrderViewModel.cs
+++ FlamingFork/ViewModels/OrderViewModel.cs
@@ -1,11 +1,18 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using FlamingFork.Models;
+using FlamingFork.Repositories.ApiServices;
 using System.Diagnostics;
 
 namespace FlamingFork.ViewModels
 {
-    public partial class OrderViewModel: ObservableObject
+    public partial class OrderViewModel : ObservableObject
     {
+        #region Properties
+
+        [ObservableProperty]
+        private List<CustomerOrderModel> _AllCustomerOrders;
+
         [ObservableProperty]
         private string _IsFetching;
 
@@ -18,17 +25,38 @@ namespace FlamingFork.ViewModels
         [ObservableProperty]
         private string _OrdersPresent;
 
+        private OrderServiceRepository _OrderServices;
+
+        #endregion Properties
+
+        #region Constructor
+
         public OrderViewModel()
         {
             _IsFetching = "False";
             _HasFetched = "True";
             _OrdersNotPresent = "False";
+            _OrdersPresent = "True";
+            _OrderServices = new OrderServiceRepository();
+            FetchCustomerOrders();
         }
+
+        #endregion Constructor
+
+        #region Methods
 
         [RelayCommand]
         public void ChangeOrderVisibilityByType()
         {
             Debug.WriteLine("Command Called!");
         }
+
+        [RelayCommand]
+        public async Task FetchCustomerOrders()
+        {
+            AllCustomerOrders = await _OrderServices.GetCustomerOrders();
+        }
+
+        #endregion Methods
     }
 }
