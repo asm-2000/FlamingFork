564531b7d5a7d966631bead156abb6f645a08f4f
asm-2000 <ashim.baral@bajratechnologies.com>
2024-08-21 10:57:01 +0545

    chore: implement additional dimensional adjustments

--- FlamingFork/Repositories/ApiServices/MenuItemFetchServiceRepository.cs
+++ FlamingFork/Repositories/ApiServices/MenuItemFetchServiceRepository.cs
@@ -14,7 +14,7 @@ namespace FlamingFork.Repositories.ApiServices
 
         public MenuItemFetchServiceRepository()
         {
-            _Address = "10.10.100.242:8080";
+            _Address = "10.10.100.53:8080";
             _HttpClient = new HttpClient();
         }
 
